4f6c108d41a231700944bff6ed7be738185e47e9
SrishtiMalik <72808536+SrishtiMalik@users.noreply.github.com>
2020-11-05 11:34:35 +0530

    Create fds.c

---
+++ fds.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+struct details
+{
+    char username[100];
+    int age;
+    char email[100];
+    char mobile[10];
+    char password[100];
+};
+
+struct hotels
+{
+   char hotel[100];
+   char food1[25];
+   char food2[25];
+   char food3[25];
+   char food4[25];
+   int first,second,third,forth;
+};
